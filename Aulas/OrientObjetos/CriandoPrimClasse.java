f38b97048cd83e623a4af496dc5d4f0d1584d8b1
Janderson <jandersonduarte@yahoo.com.br>
2025-07-12 12:06:31 -0300

    arrumando pastas e nova aula

---
+++ Aulas/OrientObjetos/CriandoPrimClasse.java
@@ -0,0 +1,18 @@
+package Aulas.OrientObjetos;
+import java.util.*;
+
+public class CriandoPrimClasse{
+    public static void main(String[] args){
+
+        var male = new Person();
+        male.setName("João");
+        male.setAge(12);
+
+        var female = new Person();
+        female.setName("Maria");
+        female.setAge(10);
+
+        System.out.println("Male name: " + male.getName() + " age: " + male.getAge());
+        System.out.println("Female name: " + female.getName() + " age: " + female.getAge());
+    }
+}
---
+++ Aulas/OrientObjetos/Person.java
@@ -0,0 +1,24 @@
+package Aulas.OrientObjetos;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    //getters
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+        //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+}
