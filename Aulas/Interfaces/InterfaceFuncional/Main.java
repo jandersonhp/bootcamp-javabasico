442cdae601bd151fef76bed9701e4d10f2b3fbec
Janderson <jandersonduarte@yahoo.com.br>
2025-07-16 15:04:10 -0300

    espaco

--- Aulas/Interfaces/InterfaceFuncional/Main.java
+++ Aulas/Interfaces/InterfaceFuncional/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         private static void printStringValue(Function<User, String> callback, List<User> users){
             users.forEach(u -> System.out.println(callback.apply(u)));
+            
         }
 }
 
