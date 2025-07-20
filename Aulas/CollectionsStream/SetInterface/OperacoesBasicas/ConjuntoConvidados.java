67e80b736d6a055f68ac772649cd3c2e197319ff
Janderson <jandersonduarte@yahoo.com.br>
2025-07-20 19:29:28 -0300

    add pesquisas set

---
+++ Aulas/CollectionsStream/SetInterface/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,53 @@
+package Aulas.CollectionsStream.SetInterface.OperacoesBasicas;
+import java.util.*;
+
+public class ConjuntoConvidados {
+    //Atributos
+    private Set<Convidado> convidadoSet;
+    //Constructor
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+    
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+    return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados \n");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados\n");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do Set de Convidados\n");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+
+}
